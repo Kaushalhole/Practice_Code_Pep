cccad21553f608d4d61994a73c67eae0c69e094c
Kaushalhole <holekaushal@gmail.com>
2022-07-24 10:20:21 +0530

    primitive

--- JavaScript/1_DataTypes/PrimitiveDT.js
+++ JavaScript/1_DataTypes/PrimitiveDT.js
@@ -1,20 +1,32 @@
-let a = 20
-console.log(a, typeof(a))
+// let a = 20
+// console.log(a, typeof(a))
 
-let b = 20.56
-console.log(b, typeof(b))
+// let b = 20.56
+// console.log(b, typeof(b))
 
-let c = "Hello i am string"
-console.log(c, typeof(c))
+// let c = "Hello i am string"
+// console.log(c, typeof(c))
 
-let d = true
-console.log(d, typeof(d))
+// let d = true
+// console.log(d, typeof(d))
 
-let e = undefined
-console.log(e, typeof(e))
+// let e = undefined
+// console.log(e, typeof(e))
 
-let f = null
-console.log(f, typeof(f))
+// let f = null
+// console.log(f, typeof(f))
+
+//---------------------------------------------------------------------------------
+
+let firstperson = 'Gaurav'
+
+let secondpersone = firstperson
+
+firstperson = 'Samad'
+
+console.log(firstperson)
+
+console.log(secondpersone)
 
 
 
